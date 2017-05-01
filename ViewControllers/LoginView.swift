489ede52c2648d1fbfeb7b68c301c41ae971f059
Anar Baghirov <a.bagirov@icloud.com>
2017-05-01 21:24:58 +0400

    Start adding to main storyboard

---
+++ ViewControllers/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  ProjectFitry
+//
+//  Created by Faannaka on 01.05.17.
+//  Copyright © 2017 Anar Baghirov. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
