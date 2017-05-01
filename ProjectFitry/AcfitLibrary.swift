072cec8bffe92db879ae7d0a0c39abdeda7e6efe
Anar Baghirov <a.bagirov@icloud.com>
2017-05-01 21:12:39 +0400

    Start LoginView

--- ProjectFitry/AcfitLibrary.swift
+++ ProjectFitry/AcfitLibrary.swift
@@ -9,25 +9,55 @@
 import Foundation
 import UIKit
 
-class AcfitLibrary {
+final class AcfitLibrary {
     private init() { }
-    
-    static let shared = AcfitLibrary()
     
     enum StatusBarColor {
         case white
         case black
     }
     
-    func animateBottomUp(animations: @escaping () -> Void) {
+    class func animateBottomUp(animations: @escaping () -> Void) {
         UIView.animate(withDuration: 2, delay: 0.5, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.2, options: [.preferredFramesPerSecond60], animations: animations, completion: nil)
     }
     
-    func set(statusBarColor color: StatusBarColor) {
+    class func animateLoginView(animations: @escaping () -> Void) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [.preferredFramesPerSecond60], animations: animations, completion: nil)
+    }
+    
+    class func set(statusBarColor color: StatusBarColor) {
         if color == StatusBarColor.black {
             UIApplication.shared.statusBarStyle = .default
         } else {
             UIApplication.shared.statusBarStyle = .lightContent
         }
+    }
+}
+
+extension UIApplication {
+    var statusBarView: UIView? {
+        return value(forKey: "statusBar") as? UIView
+    }
+}
+
+extension UIColor {
+    class var placeholderColor: UIColor {
+        return UIColor(red: 199 / 255.0, green: 199 / 255.0, blue: 205 / 255.0, alpha: 0.5)
+    }
+    
+    class var mainColor: UIColor {
+        return UIColor(red: 49 / 255.0, green: 78 / 255.0, blue: 130 / 255.0, alpha: 0.8)
+    }
+}
+
+extension UITextField {
+    func useUnderline() {
+        let border = CALayer()
+        let borderWidth = CGFloat(1.0)
+        border.borderColor = UIColor.placeholderColor.cgColor
+        border.frame = CGRect(origin: CGPoint(x: 0,y :self.frame.size.height - borderWidth), size: CGSize(width: self.frame.size.width, height: self.frame.size.height))
+        border.borderWidth = borderWidth
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
     }
 }
--- ViewControllers/LoginController.swift
+++ ViewControllers/LoginController.swift
@@ -13,6 +13,23 @@ class LoginController: UIViewController {
     @IBOutlet weak var topConstraint: NSLayoutConstraint!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var birthDateTextField: UITextField!
+    @IBOutlet weak var sexTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    @IBAction func textEditingDidBegin(_ sender: UITextField) {
+        if sender == sexTextField {
+            configure("sexTextField")
+        } else if sender == weightTextField {
+            configure("weightTextField")
+        } else if sender == heightTextField {
+            configure("heightTextField")
+        } else {
+            configure("otherTextField")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +45,6 @@ class LoginController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         configure("after")
-        
-        AcfitLibrary.shared.animateBottomUp {
-            self.view.layoutIfNeeded()
-        }
     }
     
 }
@@ -39,19 +52,65 @@ class LoginController: UIViewController {
 extension LoginController {
     func configure(_ configuration: String) {
         if configuration == "before" {
-            AcfitLibrary.shared.set(statusBarColor: .white)
+            let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+            
+            AcfitLibrary.set(statusBarColor: .white)
+            UIApplication.shared.statusBarView?.backgroundColor = .mainColor
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+            view.addGestureRecognizer(tap)
+            
+            //Configure text fields
+            [fullNameTextField, birthDateTextField, sexTextField, weightTextField, heightTextField].forEach { textField in
+                textField?.useUnderline()
+            }
             
             //Initial value for top constraint before starting animating
             topConstraint.constant = view.bounds.height
             
             //Configure loginView
-            loginView.layer.cornerRadius = 5
+            loginView.layer.cornerRadius = 10
             loginView.layer.shadowOffset = CGSize(width: 0, height: 0)
             loginView.layer.shadowColor = UIColor.black.cgColor
             loginView.layer.shadowOpacity = 0.5
         } else if configuration == "after" {
             //Starting animating
             self.topConstraint.constant = (self.view.bounds.height - self.heightConstraint.constant) / 2
+            
+            AcfitLibrary.animateBottomUp {
+                self.view.layoutIfNeeded()
+            }
+        } else if configuration == "sexTextField" {
+            self.topConstraint.constant = (self.view.bounds.height - self.heightConstraint.constant) / 4
+            
+            AcfitLibrary.animateLoginView {
+                self.view.layoutIfNeeded()
+            }
+        } else if configuration == "weightTextField" {
+            self.topConstraint.constant = -10
+            
+            AcfitLibrary.animateLoginView {
+                self.view.layoutIfNeeded()
+            }
+        } else if configuration == "heightTextField" {
+            self.topConstraint.constant = -30
+            
+            AcfitLibrary.animateLoginView {
+                self.view.layoutIfNeeded()
+            }
+        } else if configuration == "otherTextField" {
+            self.topConstraint.constant = (self.view.bounds.height - self.heightConstraint.constant) / 2
+            
+            AcfitLibrary.animateLoginView {
+                self.view.layoutIfNeeded()
+            }
         }
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func keyboardWillHide() {
+        configure("otherTextField")
     }
 }
